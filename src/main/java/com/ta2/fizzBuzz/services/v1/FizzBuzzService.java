aa6f3464d457dc7b17d499e0d4f6d7cc75cc4271
LucasFreitasRocha <rochadefreitaslucas@gmail.com>
2022-07-16 17:42:34 -0300

    refactoring logic

--- src/main/java/com/ta2/fizzBuzz/services/v1/FizzBuzzService.java
+++ src/main/java/com/ta2/fizzBuzz/services/v1/FizzBuzzService.java
@@ -9,8 +9,11 @@ import java.util.List;
 public class FizzBuzzService {
     public String singleValue(Integer value) {
         String response = "";
-
-        if( (value % 5 == 0) && (value % 3 == 0) ){
+        if((value % 3 == 0) || value.toString().contains("3")) response += "Fizz";
+        if((value % 5 == 0) || value.toString().contains("5")) response += "Buzz";
+        return (response.isEmpty()) ? value.toString() : response;
+       /* old logic
+       if( (value % 5 == 0) && (value % 3 == 0) ){
             return "FizzBuzz";
         }else if((value % 5 == 0) || value.toString().contains("5")){
             return "Buzz";
@@ -18,8 +21,7 @@ public class FizzBuzzService {
             return "Fizz";
         }else {
             return value.toString();
-        }
-
+        }*/
     }
 
     public List<String> ListValues(List<Integer> lista) {
